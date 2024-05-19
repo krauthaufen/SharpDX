54bbe477949cb51e1cca50a363d1516c0327c88a
krauthaufen <krauthaufen@awx.at>
2024-05-19 08:12:57 +0200

    sync

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-msbuild SharpDX.sln /t:Clean /m /p:Configuration=Release;Platform="Any CPU"
+dotnet msbuild SharpDX.sln /t:Clean /m /p:Configuration=Release;Platform="Any CPU"
 
-msbuild SharpDX.sln /restore /t:Build;Pack /p:Configuration=Release;Platform="Any CPU" /m
+dotnet msbuild SharpDX.sln /restore /t:Build;Pack /p:Configuration=Release;Platform="Any CPU" /m
